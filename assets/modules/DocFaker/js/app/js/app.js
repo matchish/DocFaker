cc32c550e175e76370993d50b916416e41bc4e02
Sergey <husband.sergey@gmail.com>
2013-09-08 20:22:53 +0300

    use  httpQueue service in mainCtrl

--- assets/modules/DocFaker/js/app/js/app.js
+++ assets/modules/DocFaker/js/app/js/app.js
@@ -1,4 +1,3 @@
 'use strict';
 
-// Declare app level module which depends on filters, and services
 angular.module('DocFaker', ['DocFaker.controllers', 'ui.bootstrap']);
--- assets/modules/DocFaker/js/app/js/controllers.js
+++ assets/modules/DocFaker/js/app/js/controllers.js
@@ -3,37 +3,55 @@
 /* Controllers */
 
 angular.module('DocFaker.controllers', []).
-    controller('mainCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+controller('mainCtrl', ['$scope', 'httpQueue', '$location',
+    function($scope, httpQueue, $location) {
         $scope.number = /^\d*$/;
-        $http({method: 'GET', url: $location.absUrl(), params: {action: 'get_config'}}).
-            success(function (data, status, headers, config) {
-                var formatters = [];
-                angular.forEach(data.formatters, function (formatters, provider) {
-                    angular.forEach(formatters, function (desc, name) {
-                        this.formatters.push({name: name, example: desc.example, provider: this.provider, params: desc.params});
-                    }, {formatters: this, provider: provider});
-                }, formatters);
-                data.formatters = formatters;
-                $scope.config = data;
-            }).
-            error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-    }]).
-    controller('treeCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+
+        httpQueue({
+            method: 'GET',
+            url: $location.absUrl(),
+            params: {
+                action: 'get_config'
+            }
+        }).
+        then(function(data) {
+            var formatters = [];
+            angular.forEach(data.formatters, function(formatters, provider) {
+                angular.forEach(formatters, function(desc, name) {
+                    this.formatters.push({
+                        name: name,
+                        example: desc.example,
+                        provider: this.provider,
+                        params: desc.params
+                    });
+                }, {
+                    formatters: this,
+                    provider: provider
+                });
+            }, formatters);
+            data.formatters = formatters;
+            $scope.config = data;
+        });
+    }
+]).
+controller('treeCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
         $scope.created = 0;
         $scope.errors = [];
         $scope.queue = [];
-        $scope.request = false;       
+        $scope.request = false;
         $scope.doNotDisturb = [];
-        $scope.delete = function (data) {
+        $scope.delete = function(data) {
             data.nodes = [];
         };
-        $scope.add = function (data) {
-            data.nodes.push({amount: null, template: null, nodes: []});
+        $scope.add = function(data) {
+            data.nodes.push({
+                amount: null,
+                template: null,
+                nodes: []
+            });
         };
-        $scope.desc = function (data, edit) {
+        $scope.desc = function(data, edit) {
             var desc;
             var template;
             if (edit) {
@@ -48,7 +66,7 @@ angular.module('DocFaker.controllers', []).
             return desc;
         };
         $scope.create = function create(parents, nodes) {
-            if ($scope.errors.length > 0){
+            if ($scope.errors.length > 0) {
                 return;
             }
             for (var i = 0; i < nodes.length; i++) {
@@ -59,70 +77,83 @@ angular.module('DocFaker.controllers', []).
                     doc.fields['template'] = $scope.config.templates[nodes[i].template].id;
                     doc.fields['isfolder'] = (nodes[i].nodes.length > 0) ? 1 : 0;
                     doc.amount = nodes[i].amount;
-                    $scope.queue.push({doc: doc, nodes: nodes[i].nodes});
+                    $scope.queue.push({
+                        doc: doc,
+                        nodes: nodes[i].nodes
+                    });
                 }
             }
 
-                    if (!$scope.request){
-                    var elem = $scope.queue.pop();
-                    if (elem === undefined) {return;}  
-                    (function (doc, nodes) {
-                        $scope.doNotDisturb.push(true);
-                        $scope.request = true;
-                        $http({
-                            method: 'POST',
-                            url: $location.absUrl(),
-                            params: {action: 'create_node'},
-                            data: doc
-                        }).
-                            success(function (data, status, headers, config) {
-                                $scope.request = false;
-                                $scope.doNotDisturb.pop();
-                                var parents = data;
-                                $scope.created += parents.length;
-                                create(parents, nodes);
-                            }).
-                            error(function (data, status, headers, config) {
-                                $scope.request = false;
-                                $scope.doNotDisturb.pop();
-                                $scope.errors.push("Что-то пошло не так");
-                            });
-                    })(elem.doc, elem.nodes); 
-                    }
-                    
+            if (!$scope.request) {
+                var elem = $scope.queue.pop();
+                if (elem === undefined) {
+                    return;
+                }
+                (function(doc, nodes) {
+                    $scope.doNotDisturb.push(true);
+                    $scope.request = true;
+                    $http({
+                        method: 'POST',
+                        url: $location.absUrl(),
+                        params: {
+                            action: 'create_node'
+                        },
+                        data: doc
+                    }).
+                    success(function(data, status, headers, config) {
+                        $scope.request = false;
+                        $scope.doNotDisturb.pop();
+                        var parents = data;
+                        $scope.created += parents.length;
+                        create(parents, nodes);
+                    }).
+                    error(function(data, status, headers, config) {
+                        $scope.request = false;
+                        $scope.doNotDisturb.pop();
+                        $scope.errors.push("Что-то пошло не так");
+                    });
+                })(elem.doc, elem.nodes);
+            }
+
             function get_fields(template) {
                 var fields = $scope.config.templates[template].fields;
                 fields = (fields === undefined) ? {} : fields;
                 var result = {};
-                angular.forEach(fields, function (field, id) {
+                angular.forEach(fields, function(field, id) {
                     this[field.name] = field.formatter;
                 }, result);
                 return result;
             };
         };
-        $scope.tree = [
-            {root: null, nodes: []}
-        ];
-    }]).
-    controller('configCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-        $scope.save = function () {
+        $scope.tree = [{
+            root: null,
+            nodes: []
+        }];
+    }
+]).
+controller('configCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
+        $scope.save = function() {
             $http({
                 method: 'POST',
                 url: $location.absUrl(),
-                params: {action: 'save_config'},
+                params: {
+                    action: 'save_config'
+                },
                 data: $scope.$parent.config.templates
             }).
-                success(function (data, status, headers, config) {
-                    $scope.submit_message = {
-                        type: 'alert-success',
-                        text: 'Сохранение прошло успешно'
-                    };
-                }).
-                error(function (data, status, headers, config) {
-                    $scope.submit_message = {
-                        type: 'alert-error',
-                        text: 'что-то пошло не так. Попробуйте еще раз'
-                    };
-                });
+            success(function(data, status, headers, config) {
+                $scope.submit_message = {
+                    type: 'alert-success',
+                    text: 'Сохранение прошло успешно'
+                };
+            }).
+            error(function(data, status, headers, config) {
+                $scope.submit_message = {
+                    type: 'alert-error',
+                    text: 'что-то пошло не так. Попробуйте еще раз'
+                };
+            });
         };
-    }]);
+    }
+]);
