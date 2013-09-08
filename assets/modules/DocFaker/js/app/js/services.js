da32c18d9d1e362d7074b97663051e6855901577
Sergey <husband.sergey@gmail.com>
2013-09-08 21:07:58 +0300

    register  httpQueue service

--- assets/modules/DocFaker/js/app/js/services.js
+++ assets/modules/DocFaker/js/app/js/services.js
@@ -2,28 +2,29 @@
 
 /* Services */
 
+angular.module('DocFaker', [], function($provide) {
+    $provide.factory('httpQueue', function($q, $http) {
 
-angular.factory('httpQueue', function($q, $http) {
+        var queue = [];
+        var execNext = function() {
+            var task = queue[0];
+            $http(task.c).then(function(data) {
+                queue.shift();
+                task.d.resolve(data);
+                if (queue.length > 0) execNext();
+            }, function(err) {
+                task.d.reject(err);
+            });
+        };
+        return function(config) {
+            var d = $q.defer();
+            queue.push({
+                c: config,
+                d: d
+            });
+            if (queue.length === 1) execNext();
+            return d.promise;
+        };
 
-    var queue = [];
-    var execNext = function() {
-        var task = queue[0];
-        $http(task.c).then(function(data) {
-            queue.shift();
-            task.d.resolve(data);
-            if (queue.length > 0) execNext();
-        }, function(err) {
-            task.d.reject(err);
-        });
-    };
-    return function(config) {
-        var d = $q.defer();
-        queue.push({
-            c: config,
-            d: d
-        });
-        if (queue.length === 1) execNext();
-        return d.promise;
-    };
-
+    });
 });
